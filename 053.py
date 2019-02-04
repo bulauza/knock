b29aa6ab5c580eff69a3bcbca66a9f27410286f1
Masaharu Takahashi <bulauza@gmail.com>
2019-02-04 20:44:24 +0900

    add 053.py

---
+++ 053.py
@@ -0,0 +1,19 @@
+#!/usr/bim/python3
+# cording:utf-8
+
+from pprint import pprint
+import json
+import corenlp
+
+fname = "nlp.txt"
+
+def tokenization():
+    corenlp_dir = "/home/masaharutakahashi/.local/lib/stanford-corenlp-full-2013-06-20/"
+    parser = corenlp.StanfordCoreNLP(corenlp_path=corenlp_dir)
+    
+    result = parser.parse("I am the born of my sword")
+    json_text = json.loads(result)
+    pprint(json_text)
+
+if __name__ == '__main__':
+    tokenization()
